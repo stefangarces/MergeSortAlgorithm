c263dad5ced83e78c2e5fb3c6db050108e4a82b8
Stefan Garces <stefangarces@gmail.com>
2021-02-27 22:43:28 +0100

    Add project files.

---
+++ MergeSortAlgorithm/Algorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MergeSortAlgorithm
+{
+    public static class Algorithm
+    {
+        public static void DoMergeSort(this int[] numbers)
+        {
+            var sortedNumbers = MergeSort(numbers);
+            for (int i = 0; i < sortedNumbers.Lenght; i++)
+            {
+                numbers[i] = sortedNumbers[i];
+            }
+        }
+    }
+}
